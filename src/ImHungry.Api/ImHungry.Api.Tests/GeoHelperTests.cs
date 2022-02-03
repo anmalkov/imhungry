5b45c2468fb07504a85665ee5574693c412137c7
anmalkov <an.malkov@outlook.com>
2022-02-03 12:27:37 +0100

    resolves #3

---
+++ src/ImHungry.Api/ImHungry.Api.Tests/GeoHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using ImHungry.Api.Helpers;
+using ImHungry.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ImHungry.Api.Tests;
+
+public class GeoHelperTests
+{
+    public static IEnumerable<object[]> Calculate_Distance_Data =>
+        new List<object[]>
+        {
+            new object[] { Places.Boutersem, Places.Boutersem, Unit.Kilometers, 0 },
+            new object[] { Places.Boutersem, Places.Boutersem, Unit.Miles, 0 },
+            new object[] { Places.Boutersem, Places.Kyiv, Unit.Kilometers, 1802.368, 3 },
+            new object[] { Places.Boutersem, Places.Kyiv, Unit.Miles, 1119.941, 3 }
+        };
+
+    [Theory]
+    [MemberData(nameof(Calculate_Distance_Data))]
+    public void Calculate_Distance(LatLng coordinate1, LatLng coordinate2, Unit unit, double expectedDistance, int precision = 0)
+    {
+        var distance = GeoHelper.GetDistance(coordinate1, coordinate2, unit);
+
+        Assert.Equal(expectedDistance, distance, precision);
+    }
+}
---
+++ src/ImHungry.Api/ImHungry.Api/Helpers/GeoHelper.cs
@@ -0,0 +1,36 @@
+﻿using ImHungry.Api.Models;
+
+namespace ImHungry.Api.Helpers;
+
+public enum Unit
+{
+    Miles = 1,
+    Kilometers
+}
+
+public static class GeoHelper
+{
+    public static double GetDistance(LatLng coordinate1, LatLng coordinate2, Unit unit = Unit.Kilometers)
+    {
+        var r = GetRadiusOfEarth(unit);
+        var dLat = ToRadians(coordinate2.Latitude - coordinate1.Latitude);
+        var dLon = ToRadians(coordinate2.Longitude - coordinate1.Longitude);
+        var a = Math.Sin(dLat / 2) * Math.Sin(dLat / 2) +
+                Math.Cos(ToRadians(coordinate1.Latitude)) * Math.Cos(ToRadians(coordinate2.Latitude)) *
+                Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+        var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+        var d = r * c;
+        return d;
+    }
+
+    private static double GetRadiusOfEarth(Unit units)
+    {
+        // Radius of the earth in km 6371 and in miles 3958.75587
+        return units == Unit.Kilometers ? 6371.0 : 3958.76;
+    }
+
+    private static double ToRadians(double degrees)
+    {
+        return (degrees * Math.PI) / 180;
+    }
+}
